b2f75c7ad552494bc1acdf3f0b4d273fa8ccac85
Disha Mogaveera <dishamogaveera@gmail.com>
2024-02-02 09:06:14 +0530

    new svgs for dropdown and user icons

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -90,28 +90,7 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-      <svg
-        width="40"
-        height="40"
-        viewBox="0 0 96 96"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className={styles.dropSvg}
-      >
-        <g clipPath="url(#clip0_130_21)">
-          <path
-            d="M47.3715 49.9718L48 50.4796L48.6285 49.9718L66.3593 35.6461L70.4109 38.9268L48 57.0733L25.5891 38.9268L29.6407 35.6461L47.3715 49.9718Z"
-            stroke="black"
-            strokeOpacity="0.4"
-            strokeWidth="2"
-          />
-        </g>
-        <defs>
-          <clipPath id="clip0_130_21">
-            <rect width="96" height="96" fill="white" />
-          </clipPath>
-        </defs>
-      </svg>
+      <img src="/dropdown.svg" alt="dropdown icon" className={styles.dropSvg} />
       <hr className={styles.lastrule} />
       <Footer />
     </div>
